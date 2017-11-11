19ca0246915bbe60455606cb500c9dc89f6f41f4
KOOSANGYOON <nevergiveup936@naver.com>
2017-11-11 15:32:47 +0900

    15 PUZZLE
    1. 처음에 덱을 섞는 함수 구현 완료.
    2. "X" 표시 된 블록을 안보이게 지워야 하는 부분 수정 필요.
    3. 몇 번 움직였는지(이동 횟수) 찍어줘야 함.

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -21,19 +21,12 @@ function click(arr) {
   // arr.onclick = repeat;
   for (var i = 0; i < 4; i++) {
     for (var j = 0; j < 4; j++) {
-      arr[i][j].onclick = searchAndSwap;
+      arr[i][j].onclick = search;
     }
   }
 }
 
-function repeat() {
-  var temp = [];
-  console.log(this.value, this.id);
-  temp = this.id.split("");
-  console.log(temp);
-}
-
-function searchAndSwap() {
+function search() {
   var temp;
   var sliceName= [];
   sliceName = this.id.split("");
@@ -41,45 +34,103 @@ function searchAndSwap() {
   var x = parseInt(sliceName[1]);
   var y = parseInt(sliceName[2]);
   if (x > 0 && arr[x-1][y].innerHTML === "X"){
-    //temp = this.value;
-    swapX(x, y);
-    // this.value = arr[x-1][y].value;
-    // arr[x-1][y].value = temp;
-    // arr[x][y].innerHTML = arr[x][y].value;
-    // arr[x-1][y].innerHTML = arr[x-1][y].value;
+    swapX(x, y, x - 1);
   }else if (x < 3 && arr[x+1][y].innerHTML === "X") {
-    temp = this.value;
-    this.value = arr[x+1][y].value;
-    arr[x+1][y].value = temp;
-    arr[x][y].innerHTML = arr[x][y].value;
-    arr[x+1][y].innerHTML = arr[x+1][y].value;
+    swapX(x, y, x + 1);
   }else if (y > 0 && arr[x][y-1].innerHTML === "X") {
-    temp = this.value;
-    this.value = arr[x][y-1].value;
-    arr[x][y-1].value = temp;
-    arr[x][y].innerHTML = arr[x][y].value;
-    arr[x][y-1].innerHTML = arr[x][y-1].value;
+    swapY(x, y, y - 1);
   }else if (y < 3 && arr[x][y+1].innerHTML === "X"){
-    temp = this.value;
-    this.value = arr[x][y+1].value;
-    arr[x][y+1].value = temp;
-    arr[x][y].innerHTML = arr[x][y].value;
-    arr[x][y+1].innerHTML = arr[x][y+1].value;
+    swapY(x, y, y + 1);
   }
 }
 
-function swapX(x, y) {
-  var temp = this.value;
-  this.value = arr[x-1][y].value;
-  arr[x-1][y].value = temp;
+function swapX(x, y, x2) {
+  var temp = arr[x][y].value;
+  arr[x][y].value = arr[x2][y].value;
+  arr[x2][y].value = temp;
   arr[x][y].innerHTML = arr[x][y].value;
-  arr[x-1][y].innerHTML = arr[x-1][y].value;
+  arr[x2][y].innerHTML = arr[x2][y].value;
+  countNum += 1;
 }
 function swapY(x, y, y2) {
-  var temp;
+  var temp = arr[x][y].value;
+  arr[x][y].value = arr[x][y2].value;
+  arr[x][y2].value = temp;
+  arr[x][y].innerHTML = arr[x][y].value;
+  arr[x][y2].innerHTML = arr[x][y2].value;
+  countNum += 1;
+}
+
+function randomSwap(arr, x, y, count) {
+  if (count <= 0) {
+    return;
+  }else {
+    var nextPos = chooseSwapPos(x, y);
+
+    if (nextPos === 0) {
+      randomSwap(arr, x, y, count - 1);
+    }else if (nextPos === 1) {
+      randomSwap(arr, x - 1, y, count - 1);
+    }else if (nextPos === 2) {
+      randomSwap(arr, x + 1, y, count - 1);
+    }else if (nextPos === 3) {
+      randomSwap(arr, x, y - 1, count - 1);
+    }else if (nextPos === 4) {
+      randomSwap(arr, x, y + 1, count - 1);
+    }
+  }
+}
+
+function chooseSwapPos(x, y) {
+  var posNum = parseInt(Math.ceil(Math.random() * 4));
+  if (posNum === 1) {   //UP
+    if (x - 1 < 0) {
+      return 0;
+    }
+    swapX(x, y, x - 1);
+    return 1;
+  }else if (posNum === 2) {   //DOWN
+    if (x + 1 > 3) {
+      return 0;
+    }
+    swapX(x, y, x + 1);
+    return 2;
+  }else if (posNum === 3) {   //LEFT
+    if (y - 1 < 0) {
+      return 0;
+    }
+    swapY(x, y, y - 1);
+    return 3;
+  }else if (posNum === 4) {   //RIGHT
+    if (y + 1 > 3) {
+      return 0;
+    }
+    swapY(x, y, y + 1);
+    return 4;
+  }
 }
 
 var arr = [];
 
 generateArray(arr);
+
+var countNum = 0;
+var resultStr = document.getElementById(moveNum);
+resultStr = countNum;
+
+randomSwap(arr, 3, 3, 100);
 click(arr);
+
+
+
+// #n33 {
+//   color: #2979FF;
+//   background-color: #2979FF;
+//   display: inline-block;
+//   font-size: 80px;
+//   text-align: center;
+//   height: 80px;
+//   width: 80px;
+//   border: solid #222;
+//   border-width: 0px;
+// }
